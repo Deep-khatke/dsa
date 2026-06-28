fbb725020a7b31bf2a793970a06bd906a90cedf5
Deep Khatke <deepkhatke00@gmail.com>
2026-06-28 17:03:54 +0530

    Time: 0 ms (100.00%) | Memory: 97.4 MB (24.68%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,19 @@
+//assuming every day as  selling gay 
+
+class Solution {
+public:
+    int maxProfit(vector<int>& pri) {
+        int maxprofit =0;
+        int bestbuy = pri[0];
+        int n = pri.size();
+        for(int i=1; i<n;i++){
+            if(pri[i]>bestbuy){
+                maxprofit=max(maxprofit,(pri[i]-bestbuy));
+
+            }
+            bestbuy=min(bestbuy,pri[i]);
+        }
+        return maxprofit;
+        
+    }
+};
